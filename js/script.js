3738ee2729c0b0770bc5c80920b028866d281069
Zeus5330 <Dmitry703@bigmir.net>
2023-02-02 22:11:53 +0200

    connect (script)

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
-$(document).ready(function() {
+$(document).ready(function(){
+
     $('.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 0,
-        nav: true,
-        navText: ['', ' '],
-        autoplay: true,
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+        autoplay:true,
         autoplayTimeout: 2000,
-        responsive: {
-            0: {
-                items: 1
+        responsive:{
+            0:{
+                items:1
             },
 
-            1000: {
-                items: 3
+            1000:{
+                items:3
             }
         }
     });
